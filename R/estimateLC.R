1be9fc4296af862b7a2bf6b49e4a9c6dcc92a4d2
Unknown <jonathan.heiss@mssm.edu>
2018-06-14 15:14:29 -0400

    Add variety of models to estimate leukocyte composition

--- R/estimateLC.R
+++ R/estimateLC.R
@@ -3,12 +3,15 @@
 #' @export
 #' @return Estimated cell proportions B-lymphocytes, CD4 T-cells, CD8 T-cells, Granulocytes, Monocytes and Natural killer cells using the Houseman algorithm. The model was trained on a dataset of both peripheral and cord blood, in order to apply to study populations of broad age range.
 #' @references{Houseman EA, Accomando WP, Koestler DC, Christensen BC, Marsit CJ, Nelson HH, Wiencke JK, Kelsey KT. DNA methylation arrays as surrogate measures of cell mixture distribution. BMC bioinformatics. 2012 Dec 1;13(1):86.}
+#' @references{Gervin K, Page CM, Aass HC, Jansen MA, Fjeldstad HE, Andreassen BK, Duijts L, van Meurs JB, van Zelm MC, Jaddoe VW, Nordeng H. Cell type specific DNA methylation in cord blood: A 450K-reference data set and cell count-based validation of estimated cell type composition. Epigenetics. 2016 Sep 1;11(9):690-8}
+#' @references{Reinius LE, Acevedo N, Joerink M, Pershagen G, Dahlén SE, Greco D, Söderhäll C, Scheynius A, Kere J. Differential DNA methylation in purified human blood cells: implications for cell lineage and studies on disease susceptibility. PloS one. 2012 Jul 25;7(7):e41361.}
+#' @references{de Goede OM, Razzaghian HR, Price EM, Jones MJ, Kobor MS, Robinson WP, Lavoie PM. Nucleated red blood cells impact DNA methylation and expression analyses of cord blood hematopoietic cells. Clinical epigenetics. 2015 Dec;7(1):95.}
 #' 
-estimateLC <- function(meth){
+estimateLC <- function(meth,ref){
     
     J = ncol(meth)
 
-    coefs = read.table(system.file("data/blood_coefs.txt",package="ewastools"))
+    coefs = read.table(system.file(paste0("data/",ref,".txt"),package="ewastools"))
     coefs = as.matrix(coefs)
 
     markers = match(rownames(coefs),rownames(meth))
@@ -25,7 +28,6 @@ estimateLC <- function(meth){
     EST = t(EST)
     colnames(EST) = colnames(coefs)
     EST = data.table(EST)
-
 
     return(EST)
 }
--- data-raw/setup.R
+++ data-raw/setup.R
@@ -4,6 +4,7 @@ library(magrittr)
 library(parallel)
 library(purrr)
 library(forcats)
+library(ewastools)
 
 # -------------------------------- EPIC chip manifest
 
@@ -46,6 +47,7 @@ manifest_450K = manifest_450K[,list(
     ,channel=Color_Channel
     ,next_base=Next_Base
     ,chr=CHR
+    ,mapinfo=MAPINFO
 )]
 
 manifest_450K[                          ,probe_type:="cg"]
@@ -63,74 +65,141 @@ save(manifest_epic,controls_epic,manifest_450K,controls_450K,file="../R/sysdata.
 
 # -------------------------------- Purified blood cells 
 
-dir.create("idats")
+# 450K datasets
+# test set (GSE77797)  https://doi.org/10.1186/s12859-016-0943-7
 
-# Download meta data
-meta = "https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE68456&targ=gsm&form=text&view=brief"
-meta = readLines(meta)
 
-# Split into individual samples
-meta = split(meta,cumsum(meta %like% "^\\^SAMPLE = GSM"))
+#--------------------------------------------------------------
+# de Goede (GSE68456) https://doi.org/10.1186/s13148-015-0129-6
+#--------------------------------------------------------------
 
-# Extract GSM accessions
-names(meta) = map(meta,1) %>% stri_match_first(regex="GSM\\d+")
+dir.create("GSE68456")
 
-# Parse meta data
-imap(meta,function(s,acc){
-    s = strsplit(s,split=" = ",fixed=TRUE)  
-    data.table(gsm=acc,variable=map_chr(s,1),value=map_chr(s,2))
-}) -> meta
+### Select datasets by GSE accession 
+goede = "https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE68456&targ=gsm&form=text&view=brief"
+goede %<>% map(readLines) %>% unlist
+goede = split( goede, cumsum(goede %like% "^\\^SAMPLE = GSM") )
 
-meta = rbindlist(meta)
+names(goede) =  map(goede,1) %>% stri_match_first(regex="GSM\\d+")
 
-# Keep only information on sample characteristics and supplementary files
-meta = meta[variable %chin% c("!Sample_characteristics_ch1","!Sample_supplementary_file")]
-i = meta[variable == "!Sample_characteristics_ch1",which=TRUE]
-ch = meta$value[i] %>% stri_split(fixed=": ")
-meta$variable[i] = map_chr(ch,1)
-meta$value   [i] = map_chr(ch,2)
+goede %<>% imap(function(s,acc){
+  s = strsplit(s,split=" = ",fixed=TRUE)  
+  data.table(gsm=acc,variable=map_chr(s,1),value=map_chr(s,2))
+})
+
+goede %<>% rbindlist
+
+goede = goede[variable %chin% c("!Sample_characteristics_ch1","!Sample_supplementary_file","!Sample_title")]
+
+i = goede[variable == "!Sample_characteristics_ch1",which=TRUE]
+ch = goede$value[i] %>% stri_split(fixed=": ")
+goede$variable[i] = map_chr(ch,1)
+goede$value   [i] = map_chr(ch,2)
 rm(ch)
 
+goede[variable == "!Sample_title",variable:="title"    ]
+goede[variable == "Sex"          ,variable:="sex"      ]
+goede[variable == "cell type"    ,variable:="cell_type"]
+goede = goede[!variable %in% c("facs strategy","tissue")]
+
 # Find the URLs pointing to the two .idat files
-meta[variable == "!Sample_supplementary_file" & value %like% "_Red\\.idat",variable:="red"]
-meta[variable == "!Sample_supplementary_file" & value %like% "_Grn\\.idat",variable:="grn"]
+goede[variable == "!Sample_supplementary_file" & value %like% "_Red\\.idat",variable:="red"]
+goede[variable == "!Sample_supplementary_file" & value %like% "_Grn\\.idat",variable:="grn"]
 
-# Reshape data.table from long to wide format
-meta = dcast(meta, gsm ~ variable)
+goede = dcast(goede,gsm ~ variable,fill=NA)
+goede[,sex:=factor(sex,levels=c("M","F"),labels=c("m","f"))]
+goede[,file:=paste0("GSE68456","/",gsm)]
+goede[,cell_type:=fct_recode(cell_type,B="B cell",CD4="CD4 T cell",CD8="CD8 T cell",GR="Granulocyte",MO="Monocyte",NK="NK cell",nRBC="nRBC")]
+goede[,donor:=stri_extract_last(title,regex="ind\\d+")]
 
-# Download .idat files
-# map2(meta$red, "idats/" %s+% meta$gsm %s+% "_Red.idat.gz", ~ download.file(.x,.y) )
-# map2(meta$grn, "idats/" %s+% meta$gsm %s+% "_Grn.idat.gz", ~ download.file(.x,.y) )
-meta$red = NULL; meta$grn = NULL
+# map2(goede$red, goede$file %s+% "_Red.idat.gz", ~ download.file(.x,.y) ) %>% invisible
+# map2(goede$grn, goede$file %s+% "_Grn.idat.gz", ~ download.file(.x,.y) ) %>% invisible
 
-meta[,sex:=factor(Sex,levels=c("M","F"),labels=c("m","f"))]
-meta$cell_type = fct_recode(meta$`cell type`,MO="Monocyte",NK="NK cell",CD8="CD8 T cell",CD4="CD4 T cell",GR="Granulocyte",B="B cell",nRBC="RBC nRBC")
-meta[,file:= "idats/" %s+% gsm]
-meta$tissue = "Cord Blood"
-meta = meta[,list(cell_type,tissue,sex,file)]
+goede = goede[,list(study="goede",cell_type,donor,sex,file)]
 
-cord_blood = meta
+#-----------------------------------------------------
+# Gervin https://doi.org/10.1080/15592294.2016.1214782
+#-----------------------------------------------------
+
+gervin = read.csv("gervin/sampleSheet_cordblood.csv",skip=7)
+setDT(gervin)
  
-reinius = fread("reinius/sample_sheet_IDAT.csv") 
+gervin[,cell_type:=fct_recode(cellType,B="cd19",CD4="cd4",CD8="cd8",GR="gran",MO="cd14",WB="wb")]
+gervin[,donor:=stri_extract_first(Sample_Name,regex="^\\d+")]
+gervin[,file:=paste0("gervin","/",Sentrix_ID,"_",Sentrix_Position)]
+
+gervin = gervin[,list(study="gervin",cell_type,donor,file)]
+
+#----------------------------------------------------
+# Reinius https://doi.org/10.1371/journal.pone.004136
+#----------------------------------------------------
+
+reinius = fread("reinius/sample_sheet_IDAT.csv")
+
+reinius[,donor:=stri_sub(Sample,-3,-1)]
 reinius[,`Chip Row Pos`:=stri_replace_all_fixed(`Chip Row Pos`,"O","0")] 
 reinius[,`Chip CO position`:=stri_replace_all_fixed(`Chip CO position`,"O","0")] 
 reinius[,file:=paste0("reinius/",`Chip#ID`,"_",`Chip Row Pos`,`Chip CO position`)] 
-reinius$cell_type = fct_recode(reinius$Type,MO="CD14+ Monocytes",NK="CD56+ NK-cells",CD8="CD8+ T-cells",CD4="CD4+ T-cells",GR="Granulocytes",B="CD19+ B-cells")
-reinius$tissue = "Blood (adult)"
+reinius$cell_type = fct_recode(reinius$Type,MO="CD14+ Monocytes",NK="CD56+ NK-cells",CD8="CD8+ T-cells",CD4="CD4+ T-cells",GR="Granulocytes",B="CD19+ B-cells",WB="Whole blood",EO="Eosinophils",NE="Neutrophils")
 reinius$sex = "m"
-reinius = reinius[,list(cell_type,tissue,sex,file)]
-
-purified = rbind(cord_blood,reinius); rm(cord_blood,reinius) 
-purified = purified[cell_type%in%c('MO','NK','CD8','CD4','GR','B')]
+reinius = reinius[,list(study="reinius",cell_type,donor,sex,file)]
 
 # -------------------------------- 
 
-meth = read_idats(purified$file)
-beta = meth %>% correct_dye_bias %>% dont_normalize
+purified = rbind(goede,gervin,reinius,use.names=TRUE,fill=TRUE)
+purified %<>% droplevels
+purified[,j:=1:.N]
 
-cell_types = unique(purified$cell_type) 
+meth = read_idats(purified$file) %>% correct_dye_bias %>% detectionP %>% mask(0.05)
+beta = meth %>% dont_normalize
 
-markers = list() 
+# -------------------------------- 
+train_model = function(studies,cell_types,output){
+    
+    train = purified[study %in% studies & cell_type %in% cell_types]
+    train_beta = beta[,train$j]
+
+    markers = list() 
+
+    for(ct in cell_types){ 
+        cat(ct,'\n') 
+     
+        j = train$cell_type == ct
+     
+        tmp = apply(train_beta,1,function(x){ 
+            if(!any(is.na(x[j]))) 
+            {  
+                tmp = t.test(x[j],x[!j],var.equal=T) 
+                return(c(tmp$p.value,tmp$estimate[1]-tmp$estimate[2])) 
+            }else{ 
+                return(c(NA,NA)) 
+            } 
+        }) 
+     
+        i = which(p.adjust(tmp[1,])<0.05) 
+        o = order(tmp[2,i]) 
+        markers[[ct]] = c(i[head(o,50)],i[tail(o,50)]) 
+    } 
+
+    markers %<>% unlist %>% unique 
+
+    coefs = sapply(cell_types,function(ct){ 
+        j = train$cell_type == ct
+        rowMeans(train_beta[markers,j],na.rm=TRUE) 
+    }) 
+    rownames(coefs) = rownames(train_beta)[markers] 
+    colnames(coefs) = cell_types 
+
+    write.table(coefs,file=output,row.names=TRUE) 
+}
+
+### First model using only the Gervin dataset
+train_model("gervin" ,c("GR","MO","B","CD4","CD8","NK"),"../data/gervin.txt")
+train_model("reinius",c("GR","MO","B","CD4","CD8","NK"),"../data/reinius.txt")
+train_model(c("gervin","reinius"),c("GR","MO","B","CD4","CD8","NK"),"../data/gervin+reinius.txt")
+train_model("goede",c("GR","MO","B","CD4","CD8","NK","nRBC"),"../data/goede.txt")
+
+
 
 for(ct in cell_types){ 
     cat(ct,'\n') 
