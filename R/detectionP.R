c028852f19dd87489b5a26c0c9ef30b252691322
Unknown <jonathan.heiss@mssm.edu>
2018-09-25 15:59:12 -0400

    No longer use OOB intensities to calculate detection p-values

--- R/detectionP.R
+++ R/detectionP.R
@@ -1,52 +1,125 @@
 #' @title  Detection p-values
-#' @description Compute detection p-values. p-values are based on the distribution of the intensities of the negative control probes or out-of-band intensities. \code{detP_threshold} generates a plot showing the number of undetected Y chromosome probes among male and female subjects for various p-value thresholds, in order to empirically choose a threshold. Finally, \code{mask} is masking all probes with detection p-values below the specified threshold. \code{detectionP.minfi} provides an implementation for \code{RGChannelSet} objects as used in the \code{minfi} package.
+#' @description Compute detection p-values. p-values are based on the distribution of the intensities of the negative control probes or the U (M) intensities observed for completely methylated (unmethylated) probes, respectively. \code{detP_threshold} generates a plot showing the number of undetected Y chromosome probes among male and female subjects for various p-value thresholds, in order to empirically choose a threshold. Finally, \code{mask} is masking all probes with detection p-values below the specified threshold. \code{detectionP.minfi} provides an implementation for \code{RGChannelSet} objects as used in the \code{minfi} package.
 #' @author Jonathan A. Heiss
 #' @param raw Output of calling \code{\link{read_idats}}, must include component \code{detP} for \code{mask} and \code{detP_threshold}.
 #' @param threshold p-value threshold (arithmetic scale) above which oberservations are set to NA.
 #' @param male/female Indices of male and female subjects
 #' @param rgSet minfi rgSet object 
 #' 
-#' @return For \code{detectionP}, a modified \code{raw} object with a \code{detP} component, a matrix of detection p-values, added.
-#' @return For \code{detectionP.minfi} a matrix of detection p-values.
+#' @return For \code{detectionP} a modified \code{raw} object with a \code{detP} component, a matrix of detection p-values, added.
+#' @return For \code{detectionP.minfi} and \code{detectionP.neg} a matrix of detection p-values.
 #' @return For \code{mask}, a modified \code{raw} object, with undetected probes set to \code{NA}.
 #'
+
+#' @rdname detectionP
+#'
+summits = function (beta)
+{
+    d <- density(beta,bw=0.01,na.rm=TRUE)
+
+    l = which(d$x <0.4)
+    u = which(d$x >0.6)
+
+    l = l[which.max(d$y[l])]
+    u = u[which.max(d$y[u])]
+
+    d$x[c(l,u)]
+}
+
 #' @rdname detectionP
 #' @export
-#' 
+#'
 detectionP <- function(raw){
 
-    if(!all(c('manifest','M','U','oobG','oobR')%in%names(raw))) stop('Invalid argument')
+    if(!all(c('manifest','M','U')%in%names(raw))) stop('Invalid argument')
 
     with(raw,{
 
-        bkgR = oobR$U + oobR$M
-        bkgG = oobG$U + oobG$M
-
-        muR = apply(bkgR,2,median,na.rm=TRUE)
-        sdR = apply(bkgR,2,mad   ,na.rm=TRUE)
-
-        muG = apply(bkgG,2,median,na.rm=TRUE)
-        sdG = apply(bkgG,2,mad   ,na.rm=TRUE)
-
         detP = matrix(NA_real_,nrow=nrow(U),ncol=ncol(U))
 
-         i = manifest[channel=='Red' ,index]
-            for(j in 1:ncol(M)) 
-                detP[i,j] = pnorm(U[i,j]+M[i,j],mean=muR[j],sd=sdR[j],lower.tail=FALSE)
-
-        i = manifest[channel=='Grn' ,index]
-            for(j in 1:ncol(M)) 
-                detP[i,j] = pnorm(U[i,j]+M[i,j],mean=muG[j],sd=sdG[j],lower.tail=FALSE)
+        iR = manifest[channel=="Red" ,index]
+        iG = manifest[channel=="Grn" ,index]
+        i2 = manifest[channel=="Both",index]
         
-        i = manifest[channel=='Both',index]
-            for(j in 1:ncol(M))
-                detP[i,j] = pnorm(U[i,j]+M[i,j],mean=0.5*muR[j]+0.5*muG[j],sd=sqrt(sdR[j]^2+sdG[j]^2)/2,lower.tail=FALSE)
+        for(j in 1:ncol(M)){
 
+            beta = M[,j]/(U[,j]+M[,j])
+            
+            sR = summits(beta[iR])
+            sG = summits(beta[iG])
+
+            bkgU = (beta[iR]-sR[2]) %>% abs %>% order %>% head(1000)
+            bkgM = (beta[iR]-sR[1]) %>% abs %>% order %>% head(1000)
+
+            bkgU = iR[bkgU]
+            bkgM = iR[bkgM]
+
+            muUR = median(U[bkgU,j],na.rm=TRUE)
+            muMR = median(M[bkgM,j],na.rm=TRUE)
+
+            sdUR = mad(U[bkgU,j],na.rm=TRUE)
+            sdMR = mad(M[bkgM,j],na.rm=TRUE)
+
+            bkgU = (beta[iG]-sG[2]) %>% abs %>% order %>% head(1000)
+            bkgM = (beta[iG]-sG[1]) %>% abs %>% order %>% head(1000)
+
+            bkgU = iG[bkgU]
+            bkgM = iG[bkgM]
+
+            muUG = median(U[bkgU,j],na.rm=TRUE)
+            muMG = median(M[bkgM,j],na.rm=TRUE)
+
+            sdUG = mad(U[bkgU,j],na.rm=TRUE)
+            sdMG = mad(M[bkgM,j],na.rm=TRUE)
+
+            detP[iR,j] = pnorm(U[iR,j]+M[iR,j],mean=muUR+muMR,sd=sqrt(sdUR^2+sdMR^2),lower.tail=FALSE)
+            detP[iG,j] = pnorm(U[iG,j]+M[iG,j],mean=muUG+muMG,sd=sqrt(sdUG^2+sdMG^2),lower.tail=FALSE)
+            detP[i2,j] = pnorm(U[i2,j]+M[i2,j],mean=muUR+muMG,sd=sqrt(sdUR^2+sdMG^2),lower.tail=FALSE)
+
+        }
+        
         raw$detP = detP
-
         return(raw)
     })
 }
+
+#' @rdname detectionP
+#' @export
+#'
+detectionP.neg <- function(raw){
+  
+  with(raw,{
+
+    bkgR = bkgG = controls[group=='NEGATIVE',index] 
+
+    bkgR = ctrlR[bkgR,,drop=FALSE]
+    bkgG = ctrlG[bkgG,,drop=FALSE]
+
+    muG = apply(bkgG,2,median,na.rm=TRUE)
+    sdG = apply(bkgG,2,mad   ,na.rm=TRUE)
+
+    muR = apply(bkgR,2,median,na.rm=TRUE) 
+    sdR = apply(bkgR,2,mad   ,na.rm=TRUE) 
+
+    detP = matrix(NA_real_,nrow=nrow(U),ncol=ncol(U))
+
+    i = manifest[channel=='Red' ,index] 
+        for(j in 1:ncol(M))  
+            detP[i,j] = pnorm(U[i,j]+M[i,j],mean=2*muR[j],sd=sqrt(2)*sdR[j],lower.tail=FALSE,log.p=TRUE) 
+     
+    i = manifest[channel=='Grn' ,index] 
+        for(j in 1:ncol(M))  
+            detP[i,j] = pnorm(U[i,j]+M[i,j],mean=2*muG[j],sd=sqrt(2)*sdG[j],lower.tail=FALSE,log.p=TRUE) 
+     
+    i = manifest[channel=='Both',index] 
+        for(j in 1:ncol(M)) 
+            detP[i,j] = pnorm(U[i,j]+M[i,j],mean=muR[j]+muG[j],sd=sqrt(sdR[j]^2+sdG[j]^2),lower.tail=FALSE,log.p=TRUE) 
+
+    detP/log(10)
+  })
+}
+
+
 
 #' @rdname detectionP
 #' @export
